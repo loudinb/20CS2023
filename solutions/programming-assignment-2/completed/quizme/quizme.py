986dac89a8c50055253002647f5d0896d1a243aa
Brian Loudin <loudinb@icloud.com>
2024-10-19 19:04:58 -0400

    programming assignment 2

--- solutions/programming-assignment-2/completed/quizme/quizme.py
+++ solutions/programming-assignment-2/completed/quizme/quizme.py
@@ -1,15 +1,36 @@
 import random
-
+import json
 import argparse
-
-from data.sample_questions import QUESTIONS
 from ars.arcontroller import ARController
+from pathlib import Path
 
-random.shuffle(QUESTIONS)
+# Get the current directory
 
 
-def main():
-    quiz = ARController(QUESTIONS)
+def main(name, questions_path):
+    
+    current_dir = Path.cwd()
+    questions_path = current_dir / questions_path
+    
+    # Opening JSON file
+    f = open(questions_path)
+    questions = json.load(f)
+    f.close()
+
+    random.shuffle(questions)
+
+    quiz = ARController(questions)
     quiz.start()
 
 if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description="QuizMe: An adaptive quiz application")
+    parser.add_argument("name", type=str, help="Enter your name")
+    parser.add_argument("--questions", type=str, required=True, help="Path to the question data file")
+
+    args = parser.parse_args()
+    print(args.questions)
+    main(args.name, args.questions)
+
+
+
